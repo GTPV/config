af64e8e77b85e94b593b920ec0a85fac80ef5941
GTPV <49084575+GTPV@users.noreply.github.com>
2023-12-31 14:45:53 +0900

    .emacs.d added

--- backup.sh
+++ backup.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 cp ~/.vimrc ./vim/.vimrc
-cp -R ~/.config/nvim ./nvim/nvim
+cp -R ~/.config/nvim ./neovim/
 cp ~/.tmux.conf ./tmux/.tmux.conf
 cp ~/.emacs ./emacs/.emacs
 cp -R ~/.emacs.d ./emacs/.emacs.d
